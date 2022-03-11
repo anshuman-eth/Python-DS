b834749c2fc0e2e289dfc8c2e36f4e637f2d7852
anshu <anshuman007619@gmail.com>
2022-03-12 03:13:30 +0530

    deleted unnecessary files

--- temp4.py
+++ temp4.py
@@ -1,7 +1,7 @@
-arr=[5,45,23,4]
+arr=[4,3,2,1]
 for i in range(len(arr)):
     for j in range(i+1,len(arr)):
-        if arr[i]<arr[j]:
+        if arr[i]>arr[j]:
             arr[i],arr[j]=arr[j],arr[i]
 
 print(arr)
